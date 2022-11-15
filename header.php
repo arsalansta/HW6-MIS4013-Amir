c606c5340218a724c7eb9e2a5e3f57b142c900f7
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-15 16:25:08 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="function1.html">function1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="function2.html">function2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="function3.html">function3</a>
+        </li>
+	       <li class="nav-item">
+          <a class="nav-link" href="function4.html">function4</a>
+        </li> 
+	      <li class="nav-item">
+          <a class="nav-link" href="function5.html">function5</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
