6b865d59a2411a286a33272ec50900876707859d
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-15 18:53:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HomePage HW 6</title>
+    
+     <style>
+    body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  margin: 0;
+  background: hsl(240, 56%, 98%);
+}
+  </style>
+    
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  
+  
+  
   </head>
   <body>
